faa5066827b6084dea8189dec491536915a725fc
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-06 13:53:12 +0530

    To Print Array values in normal Method

---
+++ GenericDemo/PrintArry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace GenericDemo
+{
+    class PrintArry
+    {
+        public static void ToPrintArray(int[] array)
+        {
+            foreach(int element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+        public static void ToPrintArray(double[] array)
+        {
+            foreach (double element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+        public static void ToPrintArray(char[] array)
+        {
+            foreach (char element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+
+}
+
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using GenericDemo;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nWelcome to Generics");
+        Console.WriteLine("\nPlease choose any option below");
+        Console.WriteLine("1:To Print Array");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                int[] intArray = { 20,45, 70, 10, 35 };
+                double[] doubleArray = { 30.64, 52.45, 26, 98, 98.7 };
+                char[] charArray = { 'A', 'R', 'R', 'A', 'Y' };
+                Console.WriteLine("\nInt Array values");
+                PrintArry.ToPrintArray(intArray);
+                Console.WriteLine("\nDouble Array values");
+                PrintArry.ToPrintArray(doubleArray);
+                Console.WriteLine("\nChar Array values");
+                PrintArry.ToPrintArray(charArray);
+                break;
+        }
+        Console.ReadLine();
+    }
+}
