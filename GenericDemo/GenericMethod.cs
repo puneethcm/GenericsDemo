42813a11692609b8f5c1c46d71af7a7703916c8b
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-06 14:06:37 +0530

    Generic Method

---
+++ GenericDemo/GenericMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace GenericDemo
+{
+	public class GenericMethod
+	{
+		public static void GenericMethodConcept<T>(T[] array)
+		{
+			Console.WriteLine("\nDisplaying value");
+			foreach(T elements in array)
+			{
+				Console.Write(elements + " ");
+			}
+		}
+	}
+}
+
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -5,22 +5,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        int[] intArray = { 20, 45, 70, 10, 35 };
+        double[] doubleArray = { 30.64, 52.45, 26, 98, 98.7 };
+        char[] charArray = { 'A', 'R', 'R', 'A', 'Y' };
         Console.WriteLine("\nWelcome to Generics");
         Console.WriteLine("\nPlease choose any option below");
-        Console.WriteLine("1:To Print Array");
+        Console.WriteLine("1:To Print Array\n2:Generic method concept");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
             case 1:
-                int[] intArray = { 20,45, 70, 10, 35 };
-                double[] doubleArray = { 30.64, 52.45, 26, 98, 98.7 };
-                char[] charArray = { 'A', 'R', 'R', 'A', 'Y' };
                 Console.WriteLine("\nInt Array values");
                 PrintArry.ToPrintArray(intArray);
                 Console.WriteLine("\nDouble Array values");
                 PrintArry.ToPrintArray(doubleArray);
                 Console.WriteLine("\nChar Array values");
                 PrintArry.ToPrintArray(charArray);
+                break;
+
+            case 2:
+                GenericMethod.GenericMethodConcept<int>(intArray);
+                GenericMethod.GenericMethodConcept<double>(doubleArray);
+                GenericMethod.GenericMethodConcept<char>(charArray);
                 break;
         }
         Console.ReadLine();
