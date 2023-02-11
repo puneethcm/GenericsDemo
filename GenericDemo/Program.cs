4a5818250be555b384ca63cea6b38ec208d69a30
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-11 12:27:37 +0530

    Refactor 2

--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -9,13 +9,15 @@ internal class Program
         double[] doubleArray = { 30.64, 52.45, 26, 98, 98.7 };
         char[] charArray = { 'A', 'R', 'R', 'A', 'Y' };
 
-        int a = 10, b = 20, c = 30;
+        int a = 30, b = 20, c = 10;
         float p = 12.3f, q = 5.6f, r = 40.6f;
         string str1 = "abc", str2 = "pqr", str3 = "xyz";
+
+        //int intObj=0;
         Console.WriteLine("\nWelcome to Generics");
         Console.WriteLine("\nPlease choose any option below");
         Console.WriteLine("1:To Print Array\n2:Generic method concept\n3:Generic class concept\n4:Max Integer Numbers\n5:Max Float value" +
-            "\n6:Max string value\n7:Refactor 1 Method");
+            "\n6:Max string value\n7:Refactor 1 Method\n8: Refactor 2 Class");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
@@ -67,8 +69,25 @@ internal class Program
                 Console.Write("\n{0} is greater than {1}, {2}, {3}", FindMaxValue.FindMaxValues(a, b, c), a, b, c);
                 RefactorMethod<float>.FindMaxValues(p, q, r);
                 Console.Write("\n{0} is greater than {1}, {2}, {3}", FindMaxValue.FindMaxValues(p, q, r), p, q, r);
-                FindMaxValue.FindMaxValues(str1, str2, str3);
+                RefactorMethod<string>.FindMaxValues(str1, str2, str3);
                 Console.Write("\n{0} is greater than {1}, {2}, {3}", FindMaxValue.FindMaxValues(str1, str2, str3), str1, str2, str3);
+                break;
+
+            case 8:
+                RefactorClass<int>.FindMaxValues(a, b, c);
+                Console.Write("\nInteger values are {0}, {1}, {2}", a, b, c);
+                RefactorClass<int> intValue = new RefactorClass<int>(a,b,c);
+                intValue.MaxValue();
+
+                RefactorClass<float>.FindMaxValues(p, q, r);
+                Console.Write("\nFloat values are {0}, {1}, {2}", p, q, r);
+                RefactorClass<float> floatValue = new RefactorClass<float>(p, q, r);
+                floatValue.MaxValue();
+
+                RefactorClass<string>.FindMaxValues(str1, str2, str3);
+                Console.Write("\nString values are {0}, {1}, {2}", str1, str2, str3);
+                RefactorClass<string> stringValue = new RefactorClass<string>(str1, str2, str3);
+                stringValue.MaxValue();
                 break;
         }
         Console.ReadLine();
---
+++ GenericDemo/RefactorClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace GenericDemo
+{
+    public class RefactorClass<T> where T : IComparable
+    {
+        public T firstNumb, secondNumb, thirdNumb;
+        public RefactorClass(T firstNumb,T secondNumb,T thirdNumb)
+        {
+            this.firstNumb = firstNumb;
+            this.secondNumb = secondNumb;
+            this.thirdNumb = thirdNumb;
+        }
+        public static T FindMaxValues(T firstNumb, T secondNumb, T thirdNumb)
+        {
+            if (firstNumb.CompareTo(secondNumb) > 0 && firstNumb.CompareTo(thirdNumb) > 0)
+            {
+                return firstNumb;
+            }
+            else if (secondNumb.CompareTo(firstNumb) > 0 && secondNumb.CompareTo(thirdNumb) > 0)
+            {
+                return secondNumb;
+            }
+            else if (thirdNumb.CompareTo(firstNumb) > 0 && thirdNumb.CompareTo(secondNumb) > 0)
+            {
+                return thirdNumb;
+            }
+            throw new Exception("2 or 3 value or same");
+        }
+        public void MaxValue()
+        {
+            T result = FindMaxValues(firstNumb, secondNumb, thirdNumb);
+            Console.WriteLine("\nMax Value: " + result);
+        }
+    }
+}
+
